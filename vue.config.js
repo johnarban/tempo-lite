e61f26bff23d35759608c644993df816a55f8ccb
Carifio24 <carifio24@gmail.com>
2024-04-17 15:30:25 -0400

    Add dotenv-webpack plugin.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { VuetifyPlugin } = require('webpack-plugin-vuetify');
+const DotenvWebpack = require("dotenv-webpack");
 const { defineConfig } = require("@vue/cli-service")
 const webpack = require("webpack");
 const path = require("path");
@@ -9,6 +10,13 @@ module.exports = defineConfig({
   configureWebpack: {
     plugins: [
       new VuetifyPlugin(),
+      new DotenvWebpack({
+        path: ".env",
+        systemvars: true,
+
+        // See https://stackoverflow.com/questions/67431401/conflicting-values-for-process-env-with-webpack-encore-and-dotenv
+        ignoreStub: true
+      }),
       new webpack.DefinePlugin({
         // https://github.com/vuejs/vue-cli/pull/7443
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',

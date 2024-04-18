d6695d5a2eb619feac48ed6527b986c93c71dcec
John Arban Lewis <johnarban@users.noreply.github.com>
2024-04-18 11:33:26 -0400

    add field of regard

--- src/TempoLite.vue
+++ src/TempoLite.vue
@@ -18,6 +18,7 @@
       <div id="where" class="big-label">where</div>
       <div id="map-container">
         <div id="map"></div>
+        <div id="map-legend"><hr class="line-legend">TEMPO Field of Regard</div>
         <colorbar 
           label="Amount of NO2"
           backgroundColor="transparent"
@@ -148,6 +149,7 @@
 import { defineComponent } from "vue";
 import L, { Map } from "leaflet";
 import  { cividis } from "./cividis";
+import fieldOfRegard from "./assets/TEMPO_FOR.json";
 type SheetType = "text" | "video" | null;
 type Timeout = ReturnType<typeof setTimeout>;
 
@@ -404,7 +406,19 @@ export default defineComponent({
     }).addTo(this.map as Map);
 
     this.imageOverlay.setUrl(this.imageUrl).addTo(this.map as Map);
-
+    
+   
+    L.geoJSON(
+      fieldOfRegard as GeoJSON.GeometryCollection,
+      {
+        style: {
+          color: "red",
+          fillColor: "transparent",
+          weight: 1,
+          opacity: 0.8,
+        },
+      }).addTo(this.map as Map);
+    
   },
 
   computed: {
@@ -679,6 +693,7 @@ body {
 }
 
 #map-container {
+  position: relative;
   display: flex;
   flex-direction: row;
 
@@ -686,6 +701,31 @@ body {
     flex-basis: 80%;
     flex-grow: 1;
     flex-shrink: 1;
+  }
+  
+  > #map-legend {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: fit-content;
+    z-index: 1000;
+    
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    
+    color: black;
+    background-color: #fff5;
+    padding-left: 0.5rem;
+    padding-right: 0.25rem;
+    
+    hr.line-legend {
+      display: inline-block;
+      border: 0.5px solid red;
+      width: 3rem;
+    }
+    
+
   }
 
   > .colorbar-container {

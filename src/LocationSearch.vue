8aaf0cfac578ee2530745ebb1f15c96734783900
Pat Udomprasert <pudompra@cfa.harvard.edu>
2024-10-31 10:12:36 -0400

    Shorten text so it fits in box

--- src/LocationSearch.vue
+++ src/LocationSearch.vue
@@ -138,12 +138,12 @@ export default defineComponent({
     locationLabel(): string {
       if (this.locationJustUpdated) {
         if (this.comboFocused || this.searchResults !== null || this.searchText !== null) {
-          return 'Enter city or zip code';
+          return 'Enter city or zip';
         } else {
           return this.locationUpdatedText;
         }
       } else {
-        return this.searchErrorMessage ?? 'Enter city or zip code';
+        return this.searchErrorMessage ?? 'Enter city or zip';
       }
     }
   },

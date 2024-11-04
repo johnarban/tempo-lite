92b5bae9ff42c476746dd15445da0da81ddc4de1
John Arban Lewis <johnarban@users.noreply.github.com>
2024-11-04 15:01:35 -0500

    oops...put lines back on top

--- src/TempoLite.vue
+++ src/TempoLite.vue
@@ -872,7 +872,7 @@ export default defineComponent({
       maxZoom: 20,
       attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       // crs: L.CRS.EPSG4326
-      // pane: 'labels'
+      pane: 'labels'
     }).addTo(this.map as Map);
 
     

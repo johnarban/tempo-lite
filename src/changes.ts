4964a57d6fd562a505aef66e716b2f184d55188b
John Arban Lewis <johnarban@users.noreply.github.com>
2025-10-22 11:03:53 -0400

    accidentaly replaced a past change instead of adding a new one

--- src/changes.ts
+++ src/changes.ts
@@ -6,6 +6,12 @@ export default [
     
   },
   {
+    date: "2025-04-24",
+    html: 'Educational resources designed for for informal educators (e.g., in museums, public outreach) are now available on the Cosmic Data Stories website at <a style="font-weight: normal;" tabindex="0"  href="https://www.cosmicds.cfa.harvard.edu/resources/tempo" target="_blank" rel="noopener">Educator Resources</a>. You can also find the link the menu.',
+    highlight: new Date() < new Date('2025-05-7 00:00:00') ,
+    
+  },
+  {
     date: "2025-02-02",
     html:
       'During the LA fires that began on Jan 7th, NO<sub>2</sub> levels far exceeded the default range displayed on our maps. For Jan 7 - Feb 2, 2025, we added an option to use an extended range of NO<sub>2</sub> values. For those dates, a button appears on the map to toggle between the standard and extended range.',
